f3276e6cba1f52d116c48c25d460d793e248737e
adham618 <adhamtarek291@gmail.com>
2022-02-26 20:02:01 +0200

    fix typscript error

---
+++ src/types/home.d.ts
@@ -0,0 +1,37 @@
+export interface Movie {
+  id: number;
+  attributes: {
+    title: string;
+    release_date: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    description: string;
+    poster: {
+      data: {
+        id: number;
+        attributes: {
+          formats: {
+            thumbnail: {
+              url: string;
+            };
+          };
+          url: string;
+        };
+      };
+    };
+    actors: {
+      data: [
+        {
+          id: 1;
+          attributes: {
+            createdAt: string;
+            updatedAt: string;
+            publishedAt: string;
+            actor_name: string;
+          };
+        }
+      ];
+    };
+  };
+}
