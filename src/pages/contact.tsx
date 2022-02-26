519bd7a726fc9e7693d9b31f3efff9abcd416ccd
adham618 <adhamtarek291@gmail.com>
2022-02-26 20:00:49 +0200

    create contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import Seo from '@/components/Seo';
+
+export default function ContactPage() {
+  return (
+    <>
+      <Seo templateTitle='Contact' />
+
+      <main>
+
+        <section className=''>
+          <div className='layout py-20 min-h-screen'>
+            I am contact page
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
