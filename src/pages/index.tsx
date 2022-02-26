6459bb3fa17a697e9a9c98a5371e5198e5337341
adham618 <adhamtarek291@gmail.com>
2022-02-26 11:34:01 +0200

    remove Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
   return (
-    <Layout>
+    <>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
 
@@ -14,6 +13,6 @@ export default function HomePage() {
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'></div>
         </section>
       </main>
-    </Layout>
+    </>
   );
 }
