0e151f90c41dcbbbcfdbf778fcdcf29a5a629e1b
adham618 <adhamtarek291@gmail.com>
2022-02-26 20:00:36 +0200

    Create about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import Seo from '@/components/Seo';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Seo templateTitle='About' />
+
+      <main>
+
+        <section className=''>
+          <div className='layout py-20 min-h-screen'>
+            I am about page
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
