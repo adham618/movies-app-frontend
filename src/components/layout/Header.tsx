dff491da00b08442966d7af362e5e9a69c807fb1
adham618 <adhamtarek291@gmail.com>
2022-02-26 19:59:53 +0200

    complete Header Component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,25 +1,35 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
 
+import NextImage from '../NextImage';
+
 const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/blog', label: 'Blog' },
+  { href: '/contact', label: 'Contact' },
 ];
 
 export default function Header() {
+  const router = useRouter()
   return (
-    <header className='sticky top-0 z-50 bg-white'>
+    <header className='sticky top-0 z-50 bg-gray-200 overflow-hidden shadow-slate-100'>
       <div className='layout flex h-14 items-center justify-between'>
-        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
-        </UnstyledLink>
+        <Link href="/" passHref>
+          <div className="flex cursor-pointer">
+            <NextImage src="/svg/logo.svg" width={25} height={25} alt="site-logo" draggable="false" />
+            <span className="font-bold">Movies</span>
+          </div>
+        </Link>
         <nav>
           <ul className='flex items-center justify-between space-x-4'>
             {links.map(({ href, label }) => (
               <li key={`${href}${label}`}>
                 <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
+                  <p className={router.pathname === href ? 'text-blue-500' : ''}> {label}</p>
                 </UnstyledLink>
               </li>
             ))}
