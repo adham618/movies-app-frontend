4ff24d0366a3e9b8ab5a4ce0aa690e224786575d
adham618 <adhamtarek291@gmail.com>
2022-02-27 20:04:13 +0200

    edit card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
 import * as React from 'react';
 
 
 type CardProps = {
   movie: {
+    id: number,
     attributes: {
       title: string,
       description: string;
+      uid: string,
       poster: {
         data: {
           attributes: {
@@ -21,13 +24,16 @@ type CardProps = {
 export default function Card({ movie }: CardProps) {
   const API_URL = process.env.API_URL
   return (
-    <div className='max-w-md border rounded-2xl mt-5 shadow-xl'>
+    <div className='max-w-md border rounded-2xl mt-5 shadow-xl mb-5 relative h-auto'>
       <div>
-        <img className="rounded-t-2xl w-full" src={API_URL + movie.attributes.poster.data.attributes.url} alt="card-img" />
+        <img className="rounded-t-2xl w-full h-64" src={API_URL + movie.attributes.poster.data.attributes.url} alt="card-img" />
       </div>
-      <div className="p-5">
-        <h3 className='mb-5'>{movie.attributes.title}</h3>
-        <p className='text-gray-700 leading-7'>{movie.attributes.description}</p>
+      <div className="p-3">
+        <h3 className='mb-2'>{movie.attributes.title}</h3>
+        <p className='text-gray-700 text-xs mb-5 leading-7'>{movie.attributes.description.substring(0, 70)}.....</p>
+        <Link href="/movies/[uid]" as={`/movies/${movie.attributes.uid}`}>
+          <a className='absolute bottom-2'>Watch Now</a>
+        </Link>
       </div>
     </div>
   )
