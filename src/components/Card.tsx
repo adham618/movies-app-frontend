cc96afca01299a4b39fcba070463db1d4af694ee
adham618 <adhamtarek291@gmail.com>
2022-02-28 13:29:10 +0200

    add link to img

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
 
@@ -27,17 +26,18 @@ export default function Card({ movie }: CardProps) {
   const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
   return (
     <div className='max-w-md border rounded-2xl mt-5 shadow-xl mb-5 relative h-auto'>
-      <div className='relative rounded-t-2xl w-full h-64'>
-        <Image
-          layout='fill'
-          className='rounded-t-2xl  w-full h-64'
-          priority
-          src={
-            NEXT_PUBLIC_API_URL + movie.attributes.poster.data.attributes.url
-          }
-          alt='card-img'
-        />
-      </div>
+      <Link href='/movies/[uid]' as={`/movies/${movie.attributes.uid}`} passHref>
+        <div className='cursor-pointer'>
+          <img
+            className='rounded-t-2xl w-full h-64'
+            loading='lazy'
+            src={
+              NEXT_PUBLIC_API_URL + movie.attributes.poster.data.attributes.url
+            }
+            alt='card-img'
+          />
+        </div>
+      </Link>
       <div className='p-3'>
         <h3 className='mb-2'>{movie.attributes.title}</h3>
         <p className='text-gray-700 text-xs mb-5 leading-7'>
