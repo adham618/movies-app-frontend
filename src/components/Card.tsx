b14b6091b0bcb2b570c80bd1c9762b4e50701182
adham618 <adhamtarek291@gmail.com>
2022-02-28 12:40:28 +0200

    optimize img with NextImage

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
+
+
 
 type CardProps = {
   movie: {
@@ -24,10 +27,11 @@ export default function Card({ movie }: CardProps) {
   const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
   return (
     <div className='max-w-md border rounded-2xl mt-5 shadow-xl mb-5 relative h-auto'>
-      <div>
-        <img
-          className='rounded-t-2xl w-full h-64'
-          loading='lazy'
+      <div className='relative rounded-t-2xl w-full h-64'>
+        <Image
+          layout='fill'
+          className='rounded-t-2xl  w-full h-64'
+          priority
           src={
             NEXT_PUBLIC_API_URL + movie.attributes.poster.data.attributes.url
           }
