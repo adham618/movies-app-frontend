9fa33e459a03355f361b7be84996f4bf79efe65e
adham618 <adhamtarek291@gmail.com>
2022-03-05 13:31:55 +0200

    connect about page with strapi

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,45 @@
+import { GetStaticProps } from 'next';
 import * as React from 'react';
 
 import Seo from '@/components/Seo';
-
-export default function AboutPage() {
+interface AboutPageProps {
+  about: {
+    attributes: {
+      title: string;
+      content: string;
+    };
+  };
+}
+export default function AboutPage({ about }: AboutPageProps) {
   return (
     <>
-      <Seo templateTitle='About' />
+      <Seo
+        templateTitle='About'
+        description={about.attributes.content.substring(0, 200) + '...'}
+      />
 
       <main>
         <section className=''>
-          <div className='layout py-20 min-h-screen'>I am about page</div>
+          <div className='layout min-h-screen py-6'>
+            <h1 className='mb-3 text-lg font-bold'>{about.attributes.title}</h1>
+            <p>{about.attributes.content}</p>
+          </div>
         </section>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/pages/1`);
+  const data = await res.json();
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: { about: data.data },
+  };
+};
